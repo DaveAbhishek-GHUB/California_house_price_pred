0866166fbc5b600b77a4d0abc92fd262618fdf87
Abhishek <abhishekjdave0309@gmail.com>
2025-07-29 10:52:25 +0530

    Track .pkl files with Git LFS

--- src/train.py
+++ src/train.py
@@ -9,7 +9,7 @@ def train_model(X_train, X_test, y_train, y_test, model_type='linear'):
         print("Using Linear Regression model.")
 
     elif model_type == 'random_forest':
-        model = RandomForestRegressor(n_estimators=10, max_depth=10, random_state=42)
+        model = RandomForestRegressor(n_estimators=100, random_state=42)
         print("Using random forest model.")
 
     # train the model
